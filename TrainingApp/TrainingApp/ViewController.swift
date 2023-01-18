c69df2a2e0a4a82bfef2b6a2d39a2e802fa33c69
barathrt <barath@remotetiger.com>
2023-01-18 12:08:07 -0500

    added sum of numbers

--- TrainingApp/TrainingApp/ViewController.swift
+++ TrainingApp/TrainingApp/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         
         print("Hello World")
         print("Hello")
+        sumOfNumbers(num1: 10, num2: 20)
+    }
+    
+    func sumOfNumbers(num1: Int, num2: Int) {
+        print(num1 + num2)
     }
 }
 
